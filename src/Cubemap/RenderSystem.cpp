265a433f4115f02651203d8fa55deb72f3187b33
byhj <476652422@qq.com>
2015-11-14 21:35:32 +0800

    add cubemap

---
+++ src/Cubemap/RenderSystem.cpp
@@ -0,0 +1,83 @@
+#include "RenderSystem.h"
+
+const static glm::vec3 g_LightPos(1.5f, 0.5f, 0.0f);
+
+namespace byhj
+{
+	void RenderSystem::v_InitInfo()
+	{
+		windowInfo.title += ": Cube";
+	}
+
+	void RenderSystem::v_Init()
+	{
+
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
+	
+
+		m_Skybox.Init();
+		m_Camera.SetPos( glm::vec3(0.0f, 1.0f, 3.0f) );
+	}
+
+	void RenderSystem::v_Update()
+	{
+		static GLfloat lastFrame = static_cast<float>(glfwGetTime());
+		GLfloat currentFrame = static_cast<float>(glfwGetTime());
+		GLfloat deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		m_Camera.update(deltaTime);
+	}
+
+	void RenderSystem::v_Render()
+	{
+		static const float bgColor[4] = {0.2f, 0.4f, 0.5f, 1.0f};
+		glClearBufferfv(GL_COLOR, 0, bgColor);
+		static const float one = 1.0f;
+		glClearBufferfv(GL_DEPTH, 0, &one);
+
+		static ogl::MvpMatrix matrix;
+		matrix.view  = m_Camera.GetViewMatrix();
+		matrix.proj  = glm::perspective(m_Camera.GetZoom(), GetAspect(), 0.1f, 1000.0f);
+		matrix.model = glm::mat4(1.0f);
+
+		// Draw skybox first, disable depth writing. 
+		// This way the skybox will always be drawn at the background of all the other objects.
+		glDepthMask(GL_FALSE);
+
+		m_Skybox.Render(matrix);
+
+		glDepthMask(GL_TRUE);
+		//glDepthFunc(GL_LESS); // Set depth function back to default
+
+
+	}
+
+	void RenderSystem::v_Shutdown()
+	{
+		m_Skybox.Shutdown();
+	}
+
+	/////////////////////////////////Key and Mouse//////////////////////////////////
+	void RenderSystem::v_Movement(GLFWwindow *Triangle)
+	{
+		m_Camera.movement(Triangle);
+	}
+	void RenderSystem::v_KeyCallback(GLFWwindow* Triangle, int key, int scancode, int action, int mode)
+	{
+		m_Camera.key_callback(Triangle, key, scancode, action, mode);
+	}
+
+	void RenderSystem::v_MouseCallback(GLFWwindow* Triangle, double xpos, double ypos)
+	{
+		m_Camera.mouse_callback(Triangle, xpos, ypos);
+	}
+
+	void RenderSystem::v_ScrollCallback(GLFWwindow* Triangle, double xoffset, double yoffset)
+	{
+		m_Camera.scroll_callback(Triangle, xoffset, yoffset);
+	}
+
+
+}
---
+++ src/Cubemap/cubemap.cpp
@@ -0,0 +1,150 @@
+#include "cubemap.h"
+#include "ogl/TextureMgr.h"
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+namespace byhj
+{
+#pragma region VertexData
+
+	const static GLfloat VertexData[] = {
+		// Positions          
+		-1.0f,  1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f
+
+	};
+
+#pragma endregion
+
+	void Skybox::Init()
+	{
+	
+		init_shader();
+		init_buffer();
+		init_vertexArray();
+		init_texture();
+	}
+
+	void Skybox::Render(const const ogl::MvpMatrix &matrix)
+	{
+		glUseProgram(program);
+		glBindVertexArray(vao);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap_texture); 
+
+	// Remove any translation component of the view matrix
+		glm::mat4 view  = glm::mat4( glm::mat3(matrix.view) );
+		glm::mat4 proj  = matrix.proj;
+		glm::mat4 model = matrix.model;
+		glUniformMatrix4fv(view_loc, 1, GL_FALSE,  &view[0][0]);
+		glUniformMatrix4fv(proj_loc, 1, GL_FALSE,  &proj[0][0]);
+		glUniformMatrix4fv(model_loc, 1, GL_FALSE, &model[0][0]);
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+	}
+
+	void Skybox::Shutdown()
+	{
+		glDeleteProgram(program);
+		glDeleteVertexArrays(1, &vao);
+		glDeleteBuffers(1, &vbo);
+	}
+
+	void Skybox::init_shader()
+	{
+		SkyboxShader.init();
+		SkyboxShader.attach(GL_VERTEX_SHADER,  "Skybox.vert");
+		SkyboxShader.attach(GL_FRAGMENT_SHADER, "Skybox.frag");
+		SkyboxShader.link();
+		SkyboxShader.info();
+
+		program = SkyboxShader.GetProgram();
+		model_loc = glGetUniformLocation(program, "model");
+		view_loc  = glGetUniformLocation(program, "view");
+		proj_loc  = glGetUniformLocation(program, "proj");
+		tex_loc   = glGetUniformLocation(program, "skybox");
+	}
+
+	void Skybox::init_buffer()
+	{
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);    //load the vertex data
+		glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData), VertexData, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	void Skybox::init_vertexArray()
+	{	
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(0);
+	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)* 3, (GLvoid*)(NULL));
+
+		glBindVertexArray(0);
+	}
+
+	void Skybox::init_texture()
+	{
+		m_TextureMgr.Init();
+		// Cubemap (Skybox)
+		std::vector<std::string> faces;
+		faces.push_back("/ame_siege/siege_rt.tga");
+		faces.push_back("/ame_siege/siege_lf.tga");
+		faces.push_back("/ame_siege/siege_up.tga");
+		faces.push_back("/ame_siege/siege_dn.tga");
+		faces.push_back("/ame_siege/siege_ft.tga");
+		faces.push_back("/ame_siege/siege_bk.tga");
+		cubemap_texture = m_TextureMgr.LoadTexture(faces);
+	}
+
+
+
+}//Namespace
